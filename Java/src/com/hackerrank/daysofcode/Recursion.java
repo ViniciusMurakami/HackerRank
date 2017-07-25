7da41ed786efd27a9b8400f4b4c3a9f175c75e80
ViniciusMurakami <vinicius.murakami@gmail.com>
2017-07-25 01:48:26 -0300

    Days 8-9 of 30DOC
    I've made it 2 days os lessons..

---
+++ Java/src/com/hackerrank/daysofcode/Recursion.java
@@ -0,0 +1,26 @@
+package com.hackerrank.daysofcode;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+/**
+ * Created by vmurakami on 25/07/17.
+ */
+public class Recursion {
+
+    static int factorial(int n) {
+        if(n == 1){
+            return n;
+        } else {
+            return n * factorial(n - 1) ;
+        }
+    }`
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int result = factorial(n);
+        System.out.println(result);
+    }
+}
